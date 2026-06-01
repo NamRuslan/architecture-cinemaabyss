c0033e86bce3bfd45f3d2882ed07b65eda0807bf
Ruslan Nam <ruslan.nam@mirapolis.ru>
2026-06-01 14:45:46 +0300

    task 2

---
+++ src/microservices/proxy/src/test/java/cinema/abyss/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package cinema.abyss.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
